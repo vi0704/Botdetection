553bfaf33b916921589c511f4784a3720d5dceb2
vicky0704 <vivek.vikash@codeflowtech.com>
2019-02-07 17:26:16 +0530

    initial commit

--- mysite/bots/views.py
+++ mysite/bots/views.py
@@ -1,6 +1,6 @@
 # sabse pahle 'python manage.py makemigrations' likhna niche terminal me fir 'python manage.py migrate'
-#fir python manage.py createsuperuser
-#fir runserver karna
+# fir python manage.py createsuperuser
+# fir runserver karna
 from django.shortcuts import render
 from .models import Botsdetection
 from .forms import Sessioncountform
@@ -10,20 +10,18 @@ def session_count(request):
     form = Sessioncountform(request.POST)
     if request.method == 'POST':
 
-
         if form.is_valid():
             date = form.cleaned_data['date']
             if Botsdetection.objects.filter(date=date).exists():
                 data = Botsdetection.objects.filter(date=date)
-                return render(request, 'bots/detection.html', {'data': data})
+                return render(request, 'admin/index.html', {'data': data})
             else:
-                #apna wala code yaha par date ke correspondind dal de aur jpo niche 8000 aur 5000 hai waha par returnd data de de
-                #khubsurat aur features thik kar rha tab tak
+                # apna wala code yaha par date ke correspondind dal de aur jpo niche 8000 aur 5000 hai waha par returnd data de de
+                # khubsurat aur features thik kar rha tab tak
 
-                Botsdetection.objects.create(date=date,users=8000,session_count=5000)
+                Botsdetection.objects.create(date=date, users=8000, session_count=5000)
                 data = Botsdetection.objects.filter(date=date)
-                return render(request, 'bots/detection.html',{'data': data})
+                return render(request, 'admin/index.html', {'data': data})
         else:
             form = Sessioncountform(request.POST)
-    return render(request, 'bots/form.html', {'form': form})
-
+    return render(request, 'bots/admin.html', {'form': form})
